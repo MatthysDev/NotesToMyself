0908acc2e3f26b5ea48eb8b10f07213bac1ed6f7
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2024-02-05 19:55:33 +0100

    fix: 🐛 css for menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-12 bg-slate-200">
+    <main className="flex min-h-screen flex-col items-center p-12 bg-slate-50">
       <h2 className="text-4xl font-bold flex text-black p-6">Ready to take<h2 className='text-red-500 mx-2'> notes</h2>?</h2>
       <a href='google.com' className='bg-black py-4 px-24 font-semibold mb-12'>Buy now</a>
-
       <Image
         src="/book.png"
         alt="NotesToMyself Book"
@@ -13,9 +12,6 @@ export default function Home() {
         height={240}
         priority
       />
-
-
-
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
